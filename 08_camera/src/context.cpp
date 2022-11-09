639b9a0118ad8e218f33a58a106afda6b296e422
Ahn-Ssu <ahn.ssu@kaist.ac.kr>
2022-11-09 19:11:10 +0900

    imGui 1.8.2 + chapter backup

--- 08_camera/src/context.cpp
+++ 08_camera/src/context.cpp
@@ -1,5 +1,6 @@
 #include "context.h"
 #include "image.h"
+#include <imgui.h>
 
 ContextUPtr Context::Create()
 {
@@ -106,6 +107,22 @@ uint32_t indices[] = {
 
 void Context::Render()
 {
+    if (ImGui::Begin("UI window"))    {
+        if(ImGui::ColorEdit4("clear color", glm::value_ptr(m_clearColor)))
+            glClearColor(m_clearColor.r, m_clearColor.g, m_clearColor.b, m_clearColor.a);
+    }
+    ImGui::Separator();
+    ImGui::DragFloat3("camera pos", glm::value_ptr(m_cameraPos), 0.05f);
+    ImGui::DragFloat("camear yaw", &m_cameraYaw, 0.5f);
+    ImGui::DragFloat("camear pitch", &m_cameraPitch, 0.5f, -89.0f, 89.0f);
+    ImGui::Separator();
+    if(ImGui::Button("reset camera")){
+        m_cameraYaw = 0.0f;
+        m_cameraPitch = 0.0f;
+        m_cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+    }
+    ImGui::End(); // Begin pair (one windown instance)
+
     std::vector<glm::vec3> cubePositions = {
         glm::vec3( 0.0f, 0.0f, 0.0f),
         glm::vec3( 2.0f, 5.0f, -15.0f),
