ff1061a89a67e26d22f57c6ace2dca0c96663a8b
Ahn-Ssu <ahn.ssu@kaist.ac.kr>
2022-12-21 20:34:30 +0900

    lec 08

--- 04_point/src/context.h
+++ 04_point/src/context.h
@@ -9,11 +9,12 @@
 #include "texture.h"
 
 CLASS_PTR(Context)
-class Context {
+class Context
+{
 public:
     static ContextUPtr Create();
-    void Render();  
-    void ProcessInput(GLFWwindow* window); 
+    void Render();
+    void ProcessInput(GLFWwindow *window);
     void Reshape(int width, int height);
     void MouseMove(double x, double y);
     void MouseButton(int button, int action, double x, double y);
@@ -22,6 +23,8 @@ private:
     Context() {}
     bool Init();
     ProgramUPtr m_program;
+    ProgramUPtr m_simpleProgram;
+
 
     VertexLayoutUPtr m_vertexLayout;
     BufferUPtr m_vertexBuffer;
@@ -30,19 +33,39 @@ private:
     TextureUPtr m_texture2; // if you need more texutre instance, just add the corresponeding one
 
     // camera parameter
-    bool m_cameraControl { false };
-    glm::vec2 m_prevMousePos { glm::vec2(0.0f) };
-    float m_cameraPitch { 0.0f };
-    float m_cameraYaw { 0.0f };
+    bool m_cameraControl{false};
+    glm::vec2 m_prevMousePos{glm::vec2(0.0f)};
+    float m_cameraPitch{0.0f};
+    float m_cameraYaw{0.0f};
     glm::vec3 m_cameraPos{glm::vec3(0.0f, 0.0f, 3.0f)};
     glm::vec3 m_cameraFront{glm::vec3(0.0f, 0.0f, -1.0f)}; // target, front (2-way)
     glm::vec3 m_cameraUp{glm::vec3(0.0f, 1.0f, 0.0f)};
 
-    int m_width { WINDOW_WIDTH};
-    int m_height { WINDOW_HEIGHT};
+    int m_width{WINDOW_WIDTH};
+    int m_height{WINDOW_HEIGHT};
+
+    // animation
+    bool m_animation{true};
 
     // clear color
     glm::vec4 m_clearColor{glm::vec4(0.1f, 0.2f, 0.3f, 0.0f)};
+
+    // light parameter
+    struct Light {
+        glm::vec3 position { glm::vec3(3.0f, 3.0f, 3.0f) };
+        glm::vec3 ambient { glm::vec3(0.1f, 0.1f, 0.1f) };
+        glm::vec3 diffuse { glm::vec3(0.5f, 0.5f, 0.5f) };
+        glm::vec3 specular { glm::vec3(1.0f, 1.0f, 1.0f) };
+    };
+    Light m_light;
+
+    // material parameter
+    struct Material {
+        TextureUPtr diffuse;
+        TextureUPtr specular;
+        float shininess { 32.0f };
+    };
+    Material m_material;
 };
 
 #endif // __CONTEXT_H__
